eae41b1f8a2d00dfc019f39bba0bb824f74b8dee
Gerhard Lazu <gerhard@lazu.co.uk>
2011-11-16 18:11:56 +0000

    Ordering Conflicts:
      recipes/apt.rb

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -20,10 +20,10 @@
 
 apt_repository "10gen" do
   uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
-  distribution "dist"
-  components ["10gen"]
   keyserver "keyserver.ubuntu.com"
   key "7F0CEB10"
+  distributions %w[dist]
+  components %w[10gen]
   action :add
 end
 
